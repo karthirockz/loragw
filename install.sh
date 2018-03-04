8c89266be6a3509753c79154f020dd2767b9ffaa
Karthi Ganesan <kganesan@gsngames.com>
2018-03-04 22:23:49 +0530

    initial commit

--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 set -e
+INSTALL_DIR="/opt/semtech"
 
-rm -rf /opt/semtech
-mkdir -p /opt/semtech
-cd /opt/semtech && git clone -v https://github.com/Lora-net/lora_gateway.git
-cd /opt/semtech && git clone -v https://github.com/Lora-net/packet_forwarder.git
+rm -rf $INSTALL_DIR
 
-cd /opt/semtech/lora_gateway && make all
-cd /opt/semtech/packet_forwarder && make all
+mkdir -p $INSTALL_DIR
+pushd $INSTALL_DIR
+git clone -v https://github.com/Lora-net/lora_gateway.git
+git clone -v https://github.com/Lora-net/packet_forwarder.git
 
+pushd $INSTALL_DIR/lora_gateway && make all
+popd
+pushd $INSTALL_DIR/packet_forwarder && make all
+popd
 
+#cd /opt/semtech/lora_gateway && make all
+#cd /opt/semtech/packet_forwarder && make all
+popd
+pwd
 install -m 755 /home/pi/loraserver-pi-gen/stage3/02-packet-forwarder/files/start.sh /opt/semtech/packet_forwarder/lora_pkt_fwd
 #install -m 644 /home/pi/loraserver-pi-gen/stage3/02-packet-forwarder/files/global_conf.json   ${ROOTFS_DIR}/opt/semtech/packet_forwarder/lora_pkt_fwd
 install -m 644 /home/pi/loraserver-pi-gen/stage3/02-packet-forwarder/files/packet-forwarder.service   ${ROOTFS_DIR}/lib/systemd/system/
 
 
-systemctl enable packet-forwarder
+#systemctl enable packet-forwarder
